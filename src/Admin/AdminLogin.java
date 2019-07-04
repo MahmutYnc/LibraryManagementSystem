2b87417c6d33123f78416c84fe871e454c36f743
Mahmut <mahmutla33@gmail.com>
2019-07-04 22:01:14 +0300

    origin commit

--- src/Admin/AdminLogin.java
+++ src/Admin/AdminLogin.java
@@ -1,5 +1,4 @@
 package Admin;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -11,17 +10,20 @@ public class AdminLogin {
     JButton turnBack = new JButton("");
     Dimension dimension = new Dimension(80,30);
 
+    JTextPane enterName = new JTextPane();
+    JTextPane enterPassword = new JTextPane();
+
     public AdminLogin (){
         loginButton.setSize(dimension);
         dimension.width=80;
-        dimension.height=40;
+        dimension.height=30;
         turnBack.setSize(dimension);
 
         loginPanel.setLayout(null);
         loginPanel.add(turnBack);
         turnBack.setLocation(500,20);
         try {
-            Image img = ImageIO.read(getClass().getResource("go-back-arrow.png"));
+            Image img = ImageIO.read(getClass().getResource("left-arrow-key.png"));
             turnBack.setIcon(new ImageIcon(img));
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -29,6 +31,10 @@ public class AdminLogin {
 
         loginPanel.add(loginButton);
         loginButton.setLocation(250, 250);
+
+        loginPanel.add(enterName);
+        enterName.setLocation(100,200);
+
         loginPanel.setBackground(Color.decode("#7aacfa"));
 
         loginFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
