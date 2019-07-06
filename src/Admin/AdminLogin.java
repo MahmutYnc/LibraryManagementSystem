027c5ddea1462593d6708f0e18615d57f167e6b8
Mahmut <mahmutla33@gmail.com>
2019-07-06 14:54:35 +0300

    origin commit 2

--- src/Admin/AdminLogin.java
+++ src/Admin/AdminLogin.java
@@ -8,42 +8,63 @@ public class AdminLogin {
     JFrame loginFrame = new JFrame("Login Section");
     JPanel loginPanel = new JPanel();
     JButton loginButton = new JButton("Login");
-    JButton turnBack = new JButton("");
+    JButton goBack = new JButton("");
     Dimension dimension = new Dimension(80, 30);
 
-    JTextPane enterName = new JTextPane();
-    JTextPane enterPassword = new JTextPane();
+    JLabel title = new JLabel("Admin Login Screen");
+    JLabel enterName = new JLabel("Name: ");
+    JLabel enterPassword = new JLabel("Password: ");
+
+    JTextField nameField = new JTextField("Name here!");
+    JTextField passwordField = new JTextField("Password here!");
 
     public AdminLogin() {
         loginButton.setSize(dimension);
         dimension.width = 80;
         dimension.height = 30;
-        turnBack.setSize(dimension);
-
+        goBack.setSize(dimension);
+        loginPanel.setBackground(Color.decode("#7aacfa"));
         loginPanel.setLayout(null);
-        loginPanel.add(turnBack);
-        turnBack.setLocation(500, 20);
+        loginPanel.add(goBack);
+        goBack.setLocation(500, 20);
         try {
             Image img = ImageIO.read(getClass().getResource("left-arrow-key.png"));
-            turnBack.setIcon(new ImageIcon(img));
+            goBack.setIcon(new ImageIcon(img));
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         // to remote the spacing between the image and button's borders
-        turnBack.setMargin(new Insets(0, 0, 0, 0));
+        goBack.setMargin(new Insets(0, 0, 0, 0));
         // to add a different background
-        turnBack.setBackground(Color.decode("#7aacfa"));
+        goBack.setBackground(Color.decode("#7aacfa"));
         // to remove the border
-        turnBack.setBorder(null);
-        
+        goBack.setBorder(null);
+
+
 
         loginPanel.add(loginButton);
         loginButton.setLocation(250, 250);
 
-        loginPanel.add(enterName);
-        enterName.setLocation(100, 200);
 
-        loginPanel.setBackground(Color.decode("#7aacfa"));
+        loginPanel.add(enterName);
+        enterName.setBounds(250,250,70,30);
+        enterName.setLocation(150, 150);
+        loginPanel.add(enterPassword);
+        enterPassword.setBounds(250,250,70,30);
+        enterPassword.setLocation(150, 180);
+
+        loginPanel.add(nameField);
+        nameField.setBounds(250,250,100,25);
+        nameField.setLocation(250,150);
+        loginPanel.add(passwordField);
+        passwordField.setBounds(250,250,100,25);
+        passwordField.setLocation(250,180);
+
+        Font f = new Font("serif", Font.BOLD, 34);
+        title.setFont(f);
+        loginPanel.add(title);
+        title.setBounds(250,250,500,50);
+        title.setLocation(120,50);
 
         loginFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         loginFrame.setLocation(400, 100);
